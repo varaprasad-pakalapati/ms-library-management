e452f1e1bd39d3002f28fff7cd709ce0a89ce293
Maha Satya Pakalapati <mahasatya.pakalapati@santander.co.uk>
2021-05-22 22:50:34 +0100

    Updated README.md to include documentation

--- src/main/java/com/gtf/library/entity/Member.java
+++ src/main/java/com/gtf/library/entity/Member.java
@@ -5,14 +5,11 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.util.List;
